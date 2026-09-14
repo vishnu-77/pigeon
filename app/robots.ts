cf2aabfc75ffe62a56d13c131e6b753b08a4cb41
Vishnu Prashanth <43298311+vishnu-77@users.noreply.github.com>
2026-09-14 15:57:09 +0100

    align pigeon robots host with production domain

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/"
     },
-    sitemap: "https://pigeonmq.cc/sitemap.xml",
-    host: "https://pigeonmq.cc"
+    sitemap: "https://www.pigeonmq.cc/sitemap.xml",
+    host: "https://www.pigeonmq.cc"
   };
 }
